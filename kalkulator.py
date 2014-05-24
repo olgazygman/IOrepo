e784890762f29f5e8418babe0bbe3c6382e080d7
olgazygman <olga.zygman@hotmail.com>
2014-05-24 19:23:05 +0200

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,14 +1,40 @@
 import math
 
-a = input()
-def newton(a):
-	a = a.split()
-	n = math.factorial(int(a[0]))
-	k = math.factorial(int(a[1]))
-	nk = math.factorial( int(a[0])-int(a[1]) )
-	N = n/(k*nk)
-	return int(N)
 
-print(newton(a))
-print("Plik po modyfikacji")
+
+liczby = input()
+
+def newton(a):
+
+    a = a.split()
+
+    n = math.factorial(int(a[0]))
+
+    k = math.factorial(int(a[1]))
+
+    nk = math.factorial( int(a[0])-int(a[1]))
+
+    N = n/(k*nk)
+
+    return int(N)
+
+
+
+def suma(a):
+
+    a = a.split()
+
+    x = math.factorial(int(a[0]))
+
+    y = math.factorial(int(a[1]))
+
+    sumaxy = x + y
+
+    return int(sumaxy)
+
+
+
+print(newton(liczby))
+
+print(suma(liczby))
 
